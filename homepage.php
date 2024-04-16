76ca41e6cbebc54f183cd25f9089bf5ee191db98
grace-flynn <gmf5dzw@virginia.edu>
2024-04-15 21:47:32 -0400

    Move session_start and add else
    This still won't redirect to homepage for me - don't know why

--- homepage.php
+++ homepage.php
@@ -2,3 +2,15 @@
 <!-- the recipes will be pulled from recipes other users have posted -->
 <!-- included in navigation bar as Home -->
 <!-- after login the user will be initially directed here -->
+<!-- php code to handle login -->
+<?php
+require("connect-db.php");
+?>
+
+<!-- html code for home page -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+</html>
