c7182a9e8ba6f8b265f2c829783ff09e2d559af4
juliasamaha <108821082+juliasamaha@users.noreply.github.com>
2024-04-24 14:38:42 -0400

    Grocery List Php

--- grocery-list.php
+++ grocery-list.php
@@ -23,8 +23,35 @@
     <h1>My Grocery List</h1>
 
     <ul>
-        <!-- list of items -->
+        <?php
+        //database connection parameters
+        //check connection
+        if ($conn->connect_error) {
+            die("Connection failed: ". $conn->connect_error);
+        }
+        // fetch grocery items from database
+        $sql = "SELECT ingredient FROM grocery_list";
+        $result = $conn->query($sql);
+
+        // display grocery items in unordered list
+        if ($result->num_rows > 0) {
+            while ($result->num_rows > 0) {
+                echo "<li>". $row["ingredient"]. "</li>";
+            }
+        }
+        else {
+            echo "No items found in the grocery list";
+        }
+
+        // fetch grocery items from grocery page
+        // add to unordered list
+
+
+        $conn->close();
+        ?>
     </ul>
+    <script>
+
 
 </body>
 </html>
