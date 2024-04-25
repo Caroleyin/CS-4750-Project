b464a14acfd829a0060bc87ae008eb90a00d457b
grace-flynn <gmf5dzw@virginia.edu>
2024-04-25 14:06:47 -0400

    Update homepage

--- homepage.php
+++ homepage.php
@@ -21,28 +21,38 @@
 
     <ul>
         <?php
+        require('connect-db.php');
+        session_start();
+        echo('in php');
         //database connection parameters
         //check connection
-        if (db->connect_error) {
-            die("Connection failed: ". $conn->connect_error);
+        if ($db->connect_error) {
+            die("Connection failed: ". $db->connect_error);
         }
 
-        $conn->close();
+        $db->close();
 
-        $sql = "SELECT recipe_ID, recipe_name, calories, prep_time, type_Of_Meal FROM Recipe";
-        $result = $db->query($sql);
-
+        // prepare SQL statement to fetch user based on username
+        echo('first');
+        $stmt = $db->prepare("SELECT recipe_ID, recipe_name FROM Recipe");
+        echo('second');
+        $stmt->execute();
+        echo ('a');
+        $result = $stmt->fetch();
+        echo ('b');
         // display grocery items in unordered list
-        if ($result->num_rows > 0) {
-            while ($row = fetch_assoc()) {
+        if ($result) {
+            echo ('in result');
+            while ($row = $result->fetch_assoc()) {
+                echo ('in while');
                 $recipe_ID = $row["recipe_ID"];
                 echo "<h1>". $row["recipe_name"]. "</h1>";
-                echo '<a href="recipe-info.php?val=' . $recipe_ID. '">go to recipe page!</a>';
-                echo "<ul id=recipe-list class=recipe-list>";
-                echo "<li>". $row["calories"]. "</li>";
-                echo "<li>". $row["prep_time"]. "</li>";
-                echo "<li>". $row["type_of_meal"]. "</li>";
-                "</ul>";
+                // echo '<a href="recipe-info.php?val=' . $recipe_ID. '">go to recipe page!</a>';
+                // echo "<ul id=recipe-list class=recipe-list>";
+                // echo "<li>". $row["calories"]. "</li>";
+                // echo "<li>". $row["prep_time"]. "</li>";
+                // echo "<li>". $row["type_of_meal"]. "</li>";
+                // "</ul>";
             }
         }
         else {
