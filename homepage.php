5810cbfb419c23679a0e5adc80b75316d5721839
megvandre1 <mv5vc@virginia.edu>
2024-04-25 11:26:31 -0400

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -13,4 +13,52 @@ require("connect-db.php");
 <head>
     <title>Home Page</title>
 </head>
+<<<<<<< Updated upstream
 </html>
+=======
+<body>
+    <div class="navbar">
+        <a href="homepage.php">Home</a>
+        <a href="grocery-list.php">Grocery List</a>
+        <a href="my-recipes.php">My Recipes</a>
+        <a href="profile.php">Profile</a>
+    <h1>My Homepage</h1>
+
+    <ul>
+        <?php
+        //database connection parameters
+        //check connection
+        if (db->connect_error) {
+            die("Connection failed: ". $conn->connect_error);
+        }
+
+        $conn->close();
+
+        $sql = "SELECT recipe_ID, recipe_name, calories, prep_time, type_Of_Meal FROM Recipe";
+        $result = $db->query($sql);
+
+        // display grocery items in unordered list
+        if ($result->num_rows > 0) {
+            while ($result->num_rows > 0) {
+                <ul id="recipe-list" class="recipe-list">
+                echo "<h1>". $row["recipe_name"]. "</h1>";
+                echo "<l1>". $row["calories"]. "</l1>";
+                echo "<l1>". $row["prep_time"]. "</l1>";
+                echo "<l1>". $row["type_of_meal"]. "</l1>";
+                </ul>
+            }
+        }
+        else {
+            echo "No items found in the recipe list";
+        }
+
+        $conn->close();
+
+        ?>
+    </ul>
+    <script>
+
+
+</body>
+</html>
+>>>>>>> Stashed changes
