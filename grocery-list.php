46a43aa52073dfc58d6cca4cdf4518772ad5a38a
Caroleyin <caroleyin010@gmail.com>
2024-04-25 22:11:27 -0400

    show grocery list

--- grocery-list.php
+++ grocery-list.php
@@ -5,48 +5,70 @@
 update the amounts of the groceries, and delete -->
 
 <?php
-       
-require "connect-db.php";
-session_start();
 
-// fetch grocery items from database
-$stmt = $db->prepare("SELECT * FROM Grocery_List WHERE ingredientsInList=?");
-// issue configuring with grocery list in database
-        $stmt->bindParam(1, $ingredientsInList);
-        $stmt->execute();
-        $result = $stmt->fetch(); // this works
+    require "connect-db.php";
+    session_start();
 
-// display grocery items in unordered list
-if ($result) {
-    while ($result->num_rows > 0) {
-            // fix this
-            echo "<li>". $row["ingredient"]. "</li>";
-        }
+    $username = $_SESSION["username"];
+
+    // fetch the grocery list items for a user
+    $grocery_list = getListItemsForUser($username);
+
+    // get the grocery list items for a user
+     function getListItemsForUser($username) {
+        global $db;
+        $query = "select * from (((Grocery_List natural join Create_New) natural join Contains) natural join Ingredients) where username=:username";
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(":username", $username);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+
+        return $result;
+     }
+
+    // delete item from grocery list by the ingredient_ID
+    function deleteListItem($grocery_list_ID, $ingredient_ID) {
+        global $db;
+        $query = "delete from Contains where grocery_list_ID=:grocery_list_ID and ingredient_ID=:ingredient_ID";
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(":grocery_list_ID", $grocery_list_ID);
+        $statement->bindValue(":ingredient_ID", $ingredient_ID);
+        $statement->execute();
+        $statement->closeCursor();
     }
-    else {
-        echo "No items found in the grocery list";
-    }
+?>
 
-    // for each item in grocery list
-        // display item
+    <!DOCTYPE html>
+    <html lang="en">
 
-    // delete item from list
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Grocery List</title>
+    </head>
 
-    $conn->close();
-    ?>
+    <body>
+        <div class="navbar">
+            <a href="homepage.php">Home</a>
+            <a href="grocery-list.php">Grocery List</a>
+            <a href="my-recipes.php">My Recipes</a>
+            <a href="profile.php">Profile</a>
+            <h1>My Grocery List</h1>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Grocery List</title>
-</head>
-<body>
-    <div class="navbar">
-        <a href="homepage.php">Home</a>
-        <a href="grocery-list.php">Grocery List</a>
-        <a href="my-recipes.php">My Recipes</a>
-        <a href="profile.php">Profile</a>
-    <h1>My Grocery List</h1>
-</html>
+            <!-- display grocery list items -->
+            <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+                <tr style="background-color:#B0B0B0">
+                    <th><b>Ingredient name</b></th>
+                    <th><b>Amount</b></th>
+                </tr>
+                <?php foreach ($grocery_list as $grocery_list_item): ?>
+                    <tr>
+                        <td><?php echo $grocery_list_item['name']; ?></td>
+                        <td><?php echo $grocery_list_item['amount']; ?></td>
+                    </tr>
+                <?php endforeach; ?>  
+            </table>
+    </html>
