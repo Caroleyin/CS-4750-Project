472d6db2e31bac4c87632f53f6a481f8a502e13a
Caroleyin <caroleyin010@gmail.com>
2024-04-26 22:55:59 -0400

    delete grocery list items

--- grocery-list.php
+++ grocery-list.php
@@ -12,15 +12,38 @@ update the amounts of the groceries, and delete -->
     $username = $_SESSION["username"];
 
     // fetch the grocery list items for a user
-    $grocery_list = getListItemsForUser($username);
+    $grocery_lists = getGroceryListsForUser($username);
+    // $grocery_list_items = getListItemsForUser($username);
 
-    // get the grocery list items for a user
-     function getListItemsForUser($username) {
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['deleteBtn'])) {
+            deleteListItem($_POST['groceryListID'], $_POST['ingredientID']);
+        }
+    }
+
+    // get the grocery list IDs for a user
+    function getGroceryListsForUser($username) {
         global $db;
-        $query = "select * from (((Grocery_List natural join Create_New) natural join Contains) natural join Ingredients) where username=:username";
+        $query = "select * from Create_New natural join Grocery_List where username=:username";
 
         $statement = $db->prepare($query);
         $statement->bindValue(":username", $username);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+
+        return $result;
+    }
+
+    // get the grocery list items for a user
+     function getListItemsForUser($username, $grocery_list_ID) {
+        global $db;
+        $query = "select * from (((Grocery_List natural join Create_New) natural join Contains) natural join Ingredients) where username=:username and grocery_list_ID=:grocery_list_ID";
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(":username", $username);
+        $statement->bindValue(":grocery_list_ID", $grocery_list_ID);
         $statement->execute();
         $result = $statement->fetchAll();
         $statement->closeCursor();
@@ -31,7 +54,7 @@ update the amounts of the groceries, and delete -->
     // delete item from grocery list by the ingredient_ID
     function deleteListItem($grocery_list_ID, $ingredient_ID) {
         global $db;
-        $query = "delete from Contains where grocery_list_ID=:grocery_list_ID and ingredient_ID=:ingredient_ID";
+        $query = "delete from Contains where grocery_List_ID=:grocery_list_ID and ingredient_ID=:ingredient_ID";
 
         $statement = $db->prepare($query);
         $statement->bindValue(":grocery_list_ID", $grocery_list_ID);
@@ -116,23 +139,39 @@ update the amounts of the groceries, and delete -->
             <a href="profile.php">Profile</a>
         </div>
         <div class="container">
-            <h1>My Grocery List</h1>
+            <h1>My Grocery Lists</h1>
 
-            <table class="w3-table w3-bordered w3-card-4 center">
-                <thead>
-                    <tr style="background-color:#B0B0B0">
-                        <th><b>Ingredient name</b></th>
-                        <th><b>Amount</b></th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($grocery_list as $grocery_list_item): ?>
-                    <tr>
-                        <td><?php echo $grocery_list_item['name']; ?></td>
-                        <td><?php echo $grocery_list_item['amount']; ?></td>
-                    </tr>
-                <?php endforeach; ?>  
-            </table>
+            <?php foreach ($grocery_lists as $curr_list): ?>
+                <h3><?php echo $curr_list['grocery_list_name']; ?></h3>
+                <table class="w3-table w3-bordered w3-card-4 center">
+                    <thead>
+                        <tr style="background-color:#B0B0B0">
+                            <th><b>Ingredient name</b></th>
+                            <th><b>Amount</b></th>
+                            <th><b>Bought?</b></th>
+                            <th><b>Delete?</b></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach (getListItemsForUser($username, $curr_list['grocery_List_ID']) as $grocery_list_item): ?>
+                        <tr>
+                            <td><?php echo $grocery_list_item['ingredient_Name']; ?></td>
+                            <td><?php echo $grocery_list_item['amount']; ?></td>
+                            <td><input type="checkbox" name="select"></td> 
+                            <td>
+                                <form action="grocery-list.php" method="post">   <!-- get --> 
+                                    <input type="submit" value="Delete" name="deleteBtn" 
+                                            class="btn btn-danger" />
+                                    <input type="hidden" name="groceryListID" 
+                                        value="<?php echo $grocery_list_item['grocery_List_ID']; ?>"/>
+                                    <input type="hidden" name="ingredientID" 
+                                        value="<?php echo $grocery_list_item['ingredient_ID']; ?>"/>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?> 
+                </table>
+            <?php endforeach; ?> 
         </div>
     </body>
     </html>
